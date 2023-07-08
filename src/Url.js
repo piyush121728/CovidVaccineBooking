41a40a1ee584392c4cf4204ddbe8f9fbc84e5ef0
Piyush Kumar <pk421780@gmail.com>
2023-07-08 15:33:43 +0530

    url's updated

--- src/Url.js
+++ src/Url.js
@@ -1,4 +1,4 @@
-const Url = {
+const DevUrl = {
     searchByPin : "http://localhost:80/searchcentre",
     bookCentre: "http://localhost:80/bookcentre",
     adminCreateCentre: "http://localhost:80/admin/createcentre",
@@ -6,5 +6,16 @@ const Url = {
     deleteCentre: "http://localhost:80/admin/deletecentre",
     verifyAdmin: "http://localhost:80/admin/verifyadmin"
 }
+
+const ProdUrl = {
+    searchByPin: "https://vaccinateme-server.onrender.com/searchcentre",
+    bookCentre: "https://vaccinateme-server.onrender.com/bookcentre",
+    adminCreateCentre: "https://vaccinateme-server.onrender.com/admin/createcentre",
+    fetchAllCentres: "https://vaccinateme-server.onrender.com/admin/allcentres",
+    deleteCentre: "https://vaccinateme-server.onrender.com/admin/deletecentre",
+    verifyAdmin: "https://vaccinateme-server.onrender.com/admin/verifyadmin"
+}
+
+const Url = process.env.NODE_ENV === 'production' ? ProdUrl : DevUrl;
 
 export default Url;
